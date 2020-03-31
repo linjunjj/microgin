98450802c432d05eeea275462fca3397a620ccf1
linjun <linjun@inke.cn>
2020-03-31 21:02:00 +0800

    init2

--- include/micro/Context.hpp
+++ include/micro/Context.hpp
@@ -2,6 +2,16 @@
 // Created by 林俊 on 2020/3/30.
 //
 
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/http/message.hpp>
+#include <boost/beast/http/dynamic_body.hpp>
+
 namespace microgin {
-  template
+  template <class Stream>
+  struct send_lambda {
+      Stream &stream_;
+      bool &close_;
+      boost::system
+  };
 }
