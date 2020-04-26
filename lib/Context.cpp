3b7ab9e4d11da885ed7453ba524ed3a5af38912c
linjun <linjun@inke.cn>
2020-04-26 21:54:18 +0800

    firsa

--- lib/Context.cpp
+++ lib/Context.cpp
@@ -8,9 +8,33 @@
 namespace microgin{
     static const char *arg_delim = "&";
     static const  char *pair_delim = "=";
-    class ConstextImpl: public Context {
+    class ContextImpl: public Context {
     public:
-        ConstextImpl
+        ContextImpl(boost::asio::ip::tcp::socket &socket, bool &close, boost::system::error_code &ec): _socket(socket), _close(close), _ec(ec){};
+        void  ShouldBindJSON(json &json) override;
+        void  JSON(int status, const json &json) override;
+        void ParseGetParam() override;
+
+        Dict<std::string, std::string> &GetParams() override;
+
+    private:
+        void set_parameter(boost::string_view param) override ;
+        void set_target(boost::string_view target) override;
+        void set_version(unsigned int version) override ;
+        void set_keep_alive(bool keep_alive) override ;
+        void set_body(boost::)
+
+
+    private:
+        std::string _target;
+        std::string _body;
+        std::string _parameter;
+        bool  _keep_alive{};
+        unsigned int _version{};
+        Dict<std::string, std::string> _get_dict;
+        boost::asio::ip::tcp::socket &_socket;
+        bool  &_close;
+        boost::system::error_code &_ec;
 
     };
 
